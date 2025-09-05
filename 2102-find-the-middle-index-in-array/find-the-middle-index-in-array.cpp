5ece9bd81ffe74f82e4efe795d8f81316a430ee7
Deepak chaurasia <deepakc22@iitk.ac.in>
2025-09-06 00:05:45 +0530

    Time: 0 ms (100.00%) | Memory: 15.9 MB (82.85%) - LeetSync

---
+++ 2102-find-the-middle-index-in-array/find-the-middle-index-in-array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findMiddleIndex(vector<int>& nums) {
+    int t_sum=accumulate(nums.begin(),nums.end(),0);
+    int leftsum=0;
+    int target_index=-1;
+    for(int i=0;i<nums.size();i++){
+        if(i==0){
+            leftsum=0;
+        }else{
+            leftsum+=nums[i-1];
+        }
+        if(leftsum==t_sum-leftsum-nums[i]){
+            target_index=i;
+            break;
+        }
+    }
+     return target_index;    
+    }
+};
