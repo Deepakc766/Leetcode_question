31283d6c50e5345a3e47b5f2499c4bdf4dcb6ec6
Deepak chaurasia <deepakc22@iitk.ac.in>
2025-10-06 01:31:04 +0530

    Time: 378 ms (76.43%) | Memory: 0B (100.00%) - LeetSync

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,10 +1,11 @@
-SELECT 
-    Person.firstName,
-    Person.lastName,
-    Address.city,
-    Address.state
-FROM Person
-LEFT JOIN Address
-ON Person.personId = Address.personId;
+-- SELECT 
+--     Person.firstName,
+--     Person.lastName,
+--     Address.city,
+--     Address.state
+-- FROM Person
+-- LEFT JOIN Address
+-- ON Person.personId = Address.personId;
 
+select p.firstName,p.lastName,a.city,a.state from Person as p LEFT JOIN Address as a on p.personId=a.personId
 
