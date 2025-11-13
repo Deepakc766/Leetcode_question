d6e3ae3e2ba58f9e6a1a10bdd999c994b0eaed3d
Deepak chaurasia <deepakc22@iitk.ac.in>
2025-11-13 17:43:34 +0530

    Time: 15 ms (10.94%) | Memory: 22.2 MB (6.52%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,20 +1,5 @@
 class Solution {
 public:
-bool iscycle(int src,unordered_map<int,bool> &visited,unordered_map<int,bool> &trackdfs,unordered_map<int,list<int>> &adjlist){
-    visited[src]=true;
-    trackdfs[src]=true;
-    for(auto ng:adjlist[src]){
-        if(!visited[ng]){
-            if(iscycle(ng,visited,trackdfs,adjlist)){
-                return true;
-            }
-        }else if(trackdfs[ng]){
-            return true;
-        }
-    }
-    trackdfs[src]=false;
-    return false;
-}
     bool canFinish(int n, vector<vector<int>>& p) {
         //if there is cycle then course cannot be completed 
         unordered_map<int,list<int>>adjlist;
@@ -22,16 +7,33 @@ bool iscycle(int src,unordered_map<int,bool> &visited,unordered_map<int,bool> &t
             adjlist[it[1]].push_back(it[0]);
 
         }
-        unordered_map<int,bool>visited;
-        unordered_map<int,bool>trackdfs;
-        
-        bool ans=false;
+        unordered_map<int,int>indegree;
+        queue<int>q;
         for(int i=0;i<n;i++){
-            if(!visited[i]){
-            ans=iscycle(i,visited,trackdfs,adjlist);
-            if(ans)return false;
+            for(auto ng:adjlist[i]){
+                indegree[ng]++;
             }
         }
+
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        vector<int>ans;
+        while(!q.empty()){
+            int front=q.front();
+            q.pop();
+            ans.push_back(front);
+
+        for(auto ng:adjlist[front]){
+            indegree[ng]--;
+            if(indegree[ng]==0){
+                q.push(ng);
+            }
+        }
+        }
+      if(ans.size()!=n)return false;
         return true;
 
     }
