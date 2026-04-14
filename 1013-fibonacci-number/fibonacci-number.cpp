aa304ebd7fad59b53873b3543d7446ed071c9f76
Deepak chaurasia <deepakc22@iitk.ac.in>
2026-04-14 10:30:55 +0530

    Time: 14 ms (22.82%) | Memory: 7.7 MB (88.04%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
+int recsol(int n){
+    if(n==0 | n==1)return n;
+    int ans=fib(n-1)+fib(n-2);
+    return ans;
+}
     int fib(int n) {
-        if(n==0 | n==1)return n;
-    return fib(n-1)+fib(n-2);
-        
+        return recsol(n);   
     }
 };
