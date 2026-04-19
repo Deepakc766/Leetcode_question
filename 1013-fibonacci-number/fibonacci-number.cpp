9d65b49129a648b1520078b887521145e02fa4e6
Deepak chaurasia <deepakc22@iitk.ac.in>
2026-04-19 21:56:37 +0530

    Time: 3 ms (64.27%) | Memory: 8.4 MB (10.85%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -5,7 +5,16 @@ int recfib(int n){
     int ans=recfib(n-1)+recfib(n-2);
     return ans;
 }
+
+int memo(int n,vector<int>&dp){
+    if(n==0 || n==1)return n;
+    if(dp[n]!=-1)return dp[n];
+    dp[n]=memo(n-1,dp)+memo(n-2,dp);
+    return dp[n];
+}
     int fib(int n) {
-        return recfib(n);
+        // return recfib(n);
+        vector<int>dp(n+1,-1);
+        return memo(n,dp);
     }
 };
