e552c285fda9560e6ae8c554f4cf90d813f71be7
Deepak chaurasia <deepakc22@iitk.ac.in>
2026-04-19 21:54:05 +0530

    Time: 14 ms (22.73%) | Memory: 7.6 MB (88.01%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,19 +1,11 @@
 class Solution {
 public:
-int recsol(int n){
-    if(n==0 | n==1)return n;
-    int ans=fib(n-1)+fib(n-2);
+int recfib(int n){
+    if(n==0 || n==1)return n;
+    int ans=recfib(n-1)+recfib(n-2);
     return ans;
 }
-
-int memsol(int n,vector<int>&dp){
-    if(n==0 | n==1)return n;
-    if(dp[n]!=-1)return dp[n];
-    dp[n]=fib(n-1)+fib(n-2);
-    return dp[n];
-}
     int fib(int n) {
-        vector<int>dp(n+1,-1);
-        return memsol(n,dp);   
+        return recfib(n);
     }
 };
